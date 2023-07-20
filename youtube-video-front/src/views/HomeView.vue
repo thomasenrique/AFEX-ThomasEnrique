bdcb5ba02a2bdda04c9df9ba8d1c3cc1d0e2758c
tmino <thomas.mino.97@gmail.com>
2023-07-20 16:50:47 -0400

    fix

--- youtube-video-front/src/views/HomeView.vue
+++ youtube-video-front/src/views/HomeView.vue
@@ -3,11 +3,9 @@
     <YoutubeVideoSaver />
   </div>
 </template>
-
 <script>
 // @ is an alias to /src
 import YoutubeVideoSaver from '@/components/YoutubeVideoSaver.vue'
-
 export default {
   name: 'HomeView',
   components: {
