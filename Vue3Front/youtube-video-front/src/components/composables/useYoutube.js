bb299d0c430a90b44591fffd3c9ab2a1cc75930c
tmino <thomas.mino.97@gmail.com>
2023-07-19 22:11:03 -0400

    se validan mas casos de uso como ingresas solo texto, solo falta el eliminar

--- Vue3Front/youtube-video-front/src/components/composables/useYoutube.js
+++ Vue3Front/youtube-video-front/src/components/composables/useYoutube.js
@@ -24,53 +24,59 @@ export default function () {
 
     const addVideo = async () => {
         var youtubeResult = await validarVideo(data.newVideoUrl);
+
         if (youtubeResult != null) {
-
-            //utilizar url devuelta por el api de youtube 
-            const videoResult = await fetch(youtubeResult.response.url);
-            const videoResultJson = await videoResult.json();
-            console.log(videoResultJson)
-
-            if (videoResultJson.items.length > 0) {
-                var videoFinalData = videoResultJson.items[0];
-
-                var title = videoFinalData.snippet.title;
-                var channelTitle = videoFinalData.snippet.channelTitle;
-                var description = videoFinalData.snippet.description;
-                var thumbnailsUrl = videoFinalData.snippet.thumbnails.standard.url;
-
-                var videoFinalObject = {
-                    title: title,
-                    channelTitle: channelTitle,
-                    description: description,
-                    thumbnails: thumbnailsUrl,
-                    url: data.newVideoUrl,
-                    id_share: youtubeResult.id,
-                }
-
-                const options = {
-                    method: 'POST',
-                    headers: {
-                        "content-type": "text/plain; charset=utf-8",
-                    },
-                    body: JSON.stringify(videoFinalObject)
-                }
-
-                console.log('video Final Data', videoFinalData)
-                console.log('video Final Object', videoFinalObject)
-
-                const response = await fetch(`https://opmj4tizn1.execute-api.us-east-1.amazonaws.com/Add`, options);
-                console.log(response)
-
-                data.mensajeModal = "Video guardado!"
-                openModal();
-
+            if (youtubeResult.id == "existe") {
+                data.mensajeModal = "El video ya existe."
                 data.newVideoUrl = "";
-                GetAllVideos();
+                openModal();
             } else {
-                data.mensajeModal = "El enlace no es valido"
-                openModal();
-                data.newVideoUrl = "";
+                //utilizar url devuelta por el api de youtube 
+                const videoResult = await fetch(youtubeResult.response.url);
+                const videoResultJson = await videoResult.json();
+                console.log(videoResultJson)
+
+                if (videoResultJson.items.length > 0) {
+                    var videoFinalData = videoResultJson.items[0];
+
+                    var title = videoFinalData.snippet.title;
+                    var channelTitle = videoFinalData.snippet.channelTitle;
+                    var description = videoFinalData.snippet.description;
+                    var thumbnailsUrl = videoFinalData.snippet.thumbnails.standard.url;
+
+                    var videoFinalObject = {
+                        title: title,
+                        channelTitle: channelTitle,
+                        description: description,
+                        thumbnails: thumbnailsUrl,
+                        url: data.newVideoUrl,
+                        id_share: youtubeResult.id,
+                    }
+
+                    const options = {
+                        method: 'POST',
+                        headers: {
+                            "content-type": "text/plain; charset=utf-8",
+                        },
+                        body: JSON.stringify(videoFinalObject)
+                    }
+
+                    console.log('video Final Data', videoFinalData)
+                    console.log('video Final Object', videoFinalObject)
+
+                    const response = await fetch(`https://opmj4tizn1.execute-api.us-east-1.amazonaws.com/Add`, options);
+                    console.log(response)
+
+                    data.mensajeModal = "Video guardado!"
+                    openModal();
+
+                    data.newVideoUrl = "";
+                    GetAllVideos();
+                } else {
+                    data.mensajeModal = "El enlace no es valido"
+                    openModal();
+                    data.newVideoUrl = "";
+                }
             }
         } else {
             data.mensajeModal = "El enlace no es valido"
@@ -88,22 +94,32 @@ export default function () {
     const validarVideo = async (url) => {
         try {
             const id = ObtenerId(url);
+
+            //TODO validar existencia
+
+            const existe = data.videos.filter(video => video.id_share.S == id).length == 0 ? false : true;
             console.log(id)
-            if (id.length > 0) {
-                const apiKey = 'AIzaSyCbOI5m-EgH27x0iihdn1cF-VdwfvKVJ1M';
-                const videoId = id;
-                const options = {
-                    method: 'GET', // Especificar el método HTTP que deseas utilizar
-                    headers: {
-                        "content-type": "text/plain; charset=utf-8",
-                    },
+
+            if (!existe) { /* si no existe, adelante */
+                if (id.length > 0) {
+                    const apiKey = 'AIzaSyCbOI5m-EgH27x0iihdn1cF-VdwfvKVJ1M';
+                    const videoId = id;
+                    const options = {
+                        method: 'GET', // Especificar el método HTTP que deseas utilizar
+                        headers: {
+                            "content-type": "text/plain; charset=utf-8",
+                        },
+                    }
+                    const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${apiKey}&part=snippet`, options);
+                    console.log(response)
+                    return { response: response, id: id };
+                } else {
+                    return null;
                 }
-                const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${apiKey}&part=snippet`, options);
-                console.log(response)
-                return { response: response, id: id };
             } else {
-                return null;
+                return { response: null, id: "existe" };
             }
+
 
         } catch (error) {
             return null;
